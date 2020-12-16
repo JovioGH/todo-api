c7367b7b8c229c5631e7c1ddbc3e6e5a1e7ece6f
Carlos Moreno <morenocarlos9973@gmail.com>
2020-12-15 21:31:01 -0600

    Initializes base express estructure

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const config = require('config');
+
+const app = express();
+const isProduction = process.env.NODE_ENV === 'production';
+const appPort = config.get('app.port');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(compression());
+app.use(helmet());
+app.use(morgan('combined'));
+
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use('/health', (req, res) => {
+    res.status(200).send('Todo API is alive');
+})
+
+app.listen(appPort, () => {
+    console.log(`Server listening on ${appPort}`);
+});
