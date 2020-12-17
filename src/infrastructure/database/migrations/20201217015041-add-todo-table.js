1bd05109ce0f18342a9b39aad55fe67c53f4c246
Carlos Moreno <morenocarlos9973@gmail.com>
2020-12-16 20:03:35 -0600

    Added first migration file

---
+++ src/infrastructure/database/migrations/20201217015041-add-todo-table.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    queryInterface.createTable('todo', {
+      id: {
+        type: Sequelize.INTEGER(11).UNSIGNED,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: Sequelize.STRING(255),
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING(255),
+        allowNull: false,
+        defaultValue: 'pending'
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deleted_at: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable('todo')
+};
